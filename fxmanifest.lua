41808b4bb1f3261b246a1a4a145d162eef8cb0eb
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2021-08-08 22:55:48 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,20 +4,9 @@ game 'gta5'
 description 'QB-Logs'
 version '1.0.0'
 
-ui_page 'html/index.html'
-
 shared_script '@qb-core/import.lua'
 
 server_scripts {
     'server/server.lua',
     'config.lua'
-}
-
-client_scripts {
-    'client/client.lua'
-}
-
-files {
-    'html/index.html',
-    'html/script.js'
 }
